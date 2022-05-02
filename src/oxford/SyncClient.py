b19f543b7d537e888c41e34e97cffd7bac830d6d
Oskar Manhart <52569953+PixelAgent007@users.noreply.github.com>
2022-05-02 19:24:34 +0200

    Fixed the crossReference bug inside SyncClient

--- src/oxford/SyncClient.py
+++ src/oxford/SyncClient.py
@@ -51,8 +51,12 @@ class SyncClient:
         data = self.api_request(word)
         definitions = []
         for i in data['results'][0]['lexicalEntries'][0]['entries'][0]['senses']:
-            for e in i['definitions']:
-                definitions.append(e)
+            try:
+                for e in i['definitions']:
+                    definitions.append(e)
+            except KeyError:
+                cross_reference = self.get_word_definition(i['crossReferences'][0]['text'])
+                definitions.extend(cross_reference)
 
         return definitions
 
@@ -64,9 +68,12 @@ class SyncClient:
         data = self.api_request(word)
         examples = []
         for i in data['results'][0]['lexicalEntries'][0]['entries'][0]['senses']:
-            for e in i['examples']:
-                examples.append(e['text'])
-
+            try:
+                for e in i['examples']:
+                    examples.append(e)
+            except KeyError:
+                cross_reference = self.get_word_examples(i['crossReferences'][0]['text'])
+                examples.extend(cross_reference)
         return examples
 
     def get_audio_file(self, word: str) -> str:
@@ -79,7 +86,12 @@ class SyncClient:
         """Get synonyms for the word"""
         data = self.api_request(word)
         synonyms = []
-        for i in data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['synonyms']:
-            synonyms.append(i['text'])
+        for i in data['results'][0]['lexicalEntries'][0]['entries'][0]['senses']:
+            try:
+                for e in i['synonyms']:
+                    synonyms.append(e)
+            except KeyError:
+                cross_reference = self.get_word_definition(i['crossReferences'][0]['text'])
+                synonyms.extend(cross_reference)
 
         return synonyms
